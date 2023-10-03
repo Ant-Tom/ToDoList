f38f1541cf96d359086c7689701b19b385324915
Anton <146194876+Ant-Tom@users.noreply.github.com>
2023-10-03 13:24:22 +0300

    ToDo
    Firebase

--- ToDoList/Other/ToDoListApp.swift
+++ ToDoList/Other/ToDoListApp.swift
@@ -5,13 +5,17 @@
 //  Created by Cerik on 25.09.2023.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ ToDoList/Views/RegisterView.swift
@@ -0,0 +1,47 @@
+//
+//  RegisterView.swift
+//  ToDoList
+//
+//  Created by Cerik on 25.09.2023.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @StateObject var viewModel = RegisterViewViewModel()
+    
+    
+    var body: some View {
+        VStack {
+            // Header
+            HeaderView(title: "Register", subtitle: "Start organizing todo", angle: -15, background: .orange)
+            
+            Form {
+                TextField("Full Name", text: $viewModel.name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                
+                TextField("Email Address", text: $viewModel.email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                SecureField("Password", text: $viewModel.password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Create Account", background: .green) {
+                    viewModel.register()
+                }
+                .padding()
+            }
+            .offset(y: -50)
+            Spacer()
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
